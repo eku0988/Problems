8b29e74d242077ca7100a9018af4433a83871e4f
eku0988 <ekramdesta0988@gmail.com>
2025-03-10 12:50:52 +0300

    Created 'Find the Index of the first occurence' by A2SV Hub

---
+++ 10-Mar-2025/Find the Index of the first occurence 299953.py
@@ -0,0 +1,15 @@
+# Problem: Find the Index of the first occurence - https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i + len(needle)] == needle: 
+                return i 
+            
+        return -1
