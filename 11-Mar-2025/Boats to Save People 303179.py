a621d641d550ca7ad86e1ac1ea741bfab3105cb8
eku0988 <ekramdesta0988@gmail.com>
2025-03-11 21:52:47 +0300

    Created 'Boats to Save People' by A2SV Hub

---
+++ 11-Mar-2025/Boats to Save People 303179.py
@@ -0,0 +1,20 @@
+# Problem: Boats to Save People - https://leetcode.com/problems/boats-to-save-people/
+
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        """
+        :type people: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        people.sort()
+        left=0
+        right=len(people)-1
+        count=0
+        while left<=right:
+            if people[left]+people[right]<=limit:
+                left+=1
+            right-=1
+            count+=1
+
+        return count
