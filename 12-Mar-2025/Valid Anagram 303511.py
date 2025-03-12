74bad0f0acd37eeff573f181cba416e026edf6bb
eku0988 <ekramdesta0988@gmail.com>
2025-03-12 08:52:09 +0300

    Created 'Valid Anagram' by A2SV Hub

---
+++ 12-Mar-2025/Valid Anagram 303511.py
@@ -0,0 +1,10 @@
+# Problem: Valid Anagram - https://leetcode.com/problems/valid-anagram/description/
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s) == sorted(t)
