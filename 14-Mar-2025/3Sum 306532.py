fae2b96f1f78de21b4d181fd8fa0d53b0e3a06ea
eku0988 <ekramdesta0988@gmail.com>
2025-03-14 16:08:21 +0300

    Created '3Sum' by A2SV Hub

---
+++ 14-Mar-2025/3Sum 306532.py
@@ -0,0 +1,39 @@
+# Problem: 3Sum - https://leetcode.com/problems/3sum/description/
+
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        result=[]
+        for i in range(len(nums) - 2):
+            # Skip duplicate elements
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            j = i + 1
+            k = len(nums) - 1  
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+                
+                if total == 0:
+                    result.append([nums[i], nums[j], nums[k]])
+                    
+                    # Skip duplicate numbers
+                    while j < k and nums[j] == nums[j + 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k - 1]:
+                        k -= 1
+                    
+                    j += 1
+                    k -= 1
+                
+                elif total < 0:
+                    j += 1
+                else:
+                    k -= 1
+        
+        return result
+        
