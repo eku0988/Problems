a77ea49a19d97d2f7d7344a8c861f42eea1b90fe
Ekram Desta <ekramdesta0988@gmail.com>
2025-04-15 00:39:37 +0300

    Created 'C - z-sort' by A2SV Hub

---
+++ A2SV Remote Education Contest #9 14-Apr-2025/C - z-sort 334109.py
@@ -0,0 +1,16 @@
+# Problem: C - z-sort - https://codeforces.com/gym/603156/problem/C
+
+t=int(input())
+arr= list(map(int,input().split()))
+for i in range(1, len(arr)):
+    if i % 2 == 1:
+      if arr[i] < arr[i - 1]:
+          arr[i], arr[i - 1] = arr[i - 1], arr[i]
+    else:
+      if arr[i] > arr[i - 1]:
+          arr[i], arr[i - 1] = arr[i - 1], arr[i]
+
+print(*arr)
+  
+
+  
