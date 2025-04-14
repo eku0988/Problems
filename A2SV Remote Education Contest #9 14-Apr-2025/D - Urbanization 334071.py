b5c0d59b0b54c03a487ae4bcdc33bce27f887190
Ekram Desta <ekramdesta0988@gmail.com>
2025-04-14 23:37:09 +0300

    Created 'D - Urbanization' by A2SV Hub

---
+++ A2SV Remote Education Contest #9 14-Apr-2025/D - Urbanization 334071.py
@@ -0,0 +1,13 @@
+# Problem: D - Urbanization - https://codeforces.com/gym/603156/problem/D
+
+n, n1, n2 = map(int, input().split())
+a = list(map(int, input().split()))
+
+a.sort(reverse=True)
+if n1 > n2:
+    n1, n2 = n2, n1
+sum1 = sum(a[:n1])
+sum2 = sum(a[n1:n1 + n2])
+avg = (sum1 / n1) + (sum2 / n2)
+
+print(f"{avg:.8f}")
