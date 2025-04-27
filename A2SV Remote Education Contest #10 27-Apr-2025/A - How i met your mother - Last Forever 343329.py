c24b66d467a3c8dd06218f803e3075f484943132
Ekram Desta <ekramdesta0988@gmail.com>
2025-04-27 14:29:58 +0300

    Created 'A - How i met your mother - Last Forever' by A2SV Hub

---
+++ A2SV Remote Education Contest #10 27-Apr-2025/A - How i met your mother - Last Forever 343329.py
@@ -0,0 +1,10 @@
+# Problem: A - How i met your mother - Last Forever - https://codeforces.com/gym/604857/problem/A
+
+n = int(input())
+s = input()
+
+ones = s.count('n')  
+zeros = s.count('z')  
+
+result = ['1'] * ones + ['0'] * zeros
+print(' '.join(result))
