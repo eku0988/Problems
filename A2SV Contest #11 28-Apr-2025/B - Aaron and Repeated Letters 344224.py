2bf8c14b8951f7e05c388dd1f5cc093a2c6e22a4
Ekram Desta <ekramdesta0988@gmail.com>
2025-04-28 21:27:13 +0300

    Created 'B - Aaron and Repeated Letters' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/B - Aaron and Repeated Letters 344224.py
@@ -0,0 +1,10 @@
+# Problem: B - Aaron and Repeated Letters - https://codeforces.com/gym/605795/problem/B
+
+s=input()
+stack = []
+for char in s:
+    if stack and stack[-1] == char:
+        stack.pop()  
+    else:
+        stack.append(char)
+print(''.join(stack))
