1364aacd62b8cec2f9950a1c10f9c07f7410fa84
Ekram Desta <ekramdesta0988@gmail.com>
2025-04-28 21:43:03 +0300

    Created 'C - Mr. Boomsy' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/C - Mr. Boomsy 344241.py
@@ -0,0 +1,12 @@
+# Problem: C - Mr. Boomsy - https://codeforces.com/gym/605795/problem/C
+
+t = int(input())
+for _ in range(t):
+  s = input()
+  stack=[]
+  for char in s:
+      if stack and char=='B':
+          stack.pop()  
+      else:
+          stack.append(char)
+  print(len(stack))
