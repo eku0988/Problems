9bcfb409a329243496cdffb74e2bad3e2294e59e
Ekram Desta <ekramdesta0988@gmail.com>
2025-04-28 22:35:34 +0300

    Created 'D - Electric Mayhem' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/D - Electric Mayhem 344321.py
@@ -0,0 +1,17 @@
+# Problem: D - Electric Mayhem - https://codeforces.com/gym/605795/problem/D
+
+s=input()
+stack = []
+for char in s:
+    popped = False
+    if stack and stack[-1] == char:
+      stack.pop()  
+      popped = True
+    
+    if not popped :
+      stack.append(char)
+
+if stack:
+  print("NO")
+else:
+  print("YES")
