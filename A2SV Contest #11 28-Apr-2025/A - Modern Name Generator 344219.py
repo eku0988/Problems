646a07d64dbb5a01da9709f2ea980cf09f418420
Ekram Desta <ekramdesta0988@gmail.com>
2025-04-28 21:23:11 +0300

    Created 'A - Modern Name Generator' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/A - Modern Name Generator 344219.py
@@ -0,0 +1,7 @@
+# Problem: A - Modern Name Generator - https://codeforces.com/gym/605795/problem/A
+
+t = int(input())
+for _ in range(t):
+  words = input().split()
+  output=words[0][0] + words[1][0] + words[2][0] 
+  print(output.lower())
