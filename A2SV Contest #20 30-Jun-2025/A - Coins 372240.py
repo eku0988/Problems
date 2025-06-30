02e6745d8ba18c0689046778709e7d55cadcd29a
Ekram Desta <ekramdesta0988@gmail.com>
2025-06-30 23:18:56 +0300

    Created 'A - Coins' by A2SV Hub

---
+++ A2SV Contest #20 30-Jun-2025/A - Coins 372240.py
@@ -0,0 +1,14 @@
+# Problem: A - Coins - https://codeforces.com/gym/618729/problem/A
+
+t = int(input())
+for _ in range(t):
+  n, k = map(int, input().split())
+  max_y = n // k
+  found = False
+  # Try y = max_y down to a few lower values (at most 100 tries)
+  for i in range(max(0, max_y - 100), max_y + 1):
+      rem = n - i * k
+      if rem >= 0 and rem % 2 == 0:
+          found = True
+          break
+  print("YES" if found else "NO")
