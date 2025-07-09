3b2fa71f9db16454eaff5d446a3b0ec45da757d1
Ekram Desta <ekramdesta0988@gmail.com>
2025-07-09 21:30:56 +0300

    Created 'Course Schedule' by A2SV Hub

---
+++ 09-Jul-2025/Course Schedule 374888.py
@@ -0,0 +1,28 @@
+# Problem: Course Schedule - https://leetcode.com/problems/course-schedule/
+
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        graph = defaultdict(list)
+        in_degree = [0] * numCourses
+
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+            in_degree[course] += 1
+
+        queue = deque([i for i in range(numCourses) if in_degree[i] == 0])
+        taken_courses = 0
+
+        while queue:
+            course = queue.popleft()
+            taken_courses += 1
+            for neighbor in graph[course]:
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
+                    queue.append(neighbor)
+
+        return taken_courses == numCourses
