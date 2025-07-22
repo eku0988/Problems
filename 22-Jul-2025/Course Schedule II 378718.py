efd4b47cc86d133788df03ad2cefb900d7cecc16
Ekram Desta <ekramdesta0988@gmail.com>
2025-07-22 22:53:28 +0300

    Created 'Course Schedule II' by A2SV Hub

---
+++ 22-Jul-2025/Course Schedule II 378718.py
@@ -0,0 +1,37 @@
+# Problem: Course Schedule II - https://leetcode.com/problems/course-schedule-ii/description/
+
+from collections import deque
+
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        graph = [[] for _ in range(numCourses)]
+        incoming = [0 for _ in range(numCourses)]
+        queue = deque()
+        order = []
+
+        for course, pre in prerequisites:
+            graph[pre].append(course)
+            incoming[course] += 1
+
+        for course in range(numCourses):
+            if incoming[course] == 0:
+                queue.append(course)
+
+        while queue:
+            course = queue.popleft()
+            order.append(course)
+
+            for neighbor in graph[course]:
+                incoming[neighbor] -= 1
+                if incoming[neighbor] == 0:
+                    queue.append(neighbor)
+
+        if len(order) != numCourses:
+            return []
+
+        return order
