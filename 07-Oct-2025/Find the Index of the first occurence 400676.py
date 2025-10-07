c9f7fc8d03034914e07a5262ae10f6a4611475c7
Ekram Desta <ekramdesta0988@gmail.com>
2025-10-07 21:48:44 +0300

    Created 'Find the Index of the first occurence' by A2SV Hub

---
+++ 07-Oct-2025/Find the Index of the first occurence 400676.py
@@ -0,0 +1,20 @@
+# Problem: Find the Index of the first occurence - https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) > len(haystack):
+            return -1
+
+        i = j = 0
+        while i < len(haystack):
+            ans = i
+            while i < len(haystack) and haystack[i] == needle[j]:
+                i += 1
+                j += 1
+
+                if j == len(needle):
+                    return ans
+
+            i = ans + 1
+            j = 0
+        return -1
